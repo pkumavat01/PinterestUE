00b4f27c55afb9ba588b700d15bc3cc549742575
pkumavat01 <pkumavat@adobe.com>
2025-06-30 15:11:10 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,23 +2,43 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
   const ul = document.createElement('ul');
+
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
+
+    const cardDivs = [...row.children];
     while (row.firstElementChild) li.append(row.firstElementChild);
+
+    // Assign classes to children
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+      } else {
+        div.className = 'cards-card-body';
+      }
     });
+
+    // Extract and apply custom card variation class (e.g., 'big-card')
+    const classDiv = cardDivs[cardDivs.length - 1]; // Last div has the classes
+    if (classDiv && classDiv.textContent.includes(',')) {
+      const classes = classDiv.textContent.split(',').map(s => s.trim()); // ['card', 'big-card']
+      classes.forEach((cls) => {
+        if (cls && cls !== 'card') li.classList.add(cls); // Add only meaningful classes
+      });
+    }
+
     ul.append(li);
   });
+
+  // Replace images with optimized versions
   ul.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     moveInstrumentation(img, optimizedPic.querySelector('img'));
     img.closest('picture').replaceWith(optimizedPic);
   });
+
   block.textContent = '';
   block.append(ul);
 }
