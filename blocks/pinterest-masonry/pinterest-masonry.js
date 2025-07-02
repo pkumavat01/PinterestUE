f4ebd833531840c229e7483b8da50d4eb1144a60
pkumavat01 <pkumavat@adobe.com>
2025-07-02 12:22:49 +0530

    minor updates

--- blocks/pinterest-masonry/pinterest-masonry.js
+++ blocks/pinterest-masonry/pinterest-masonry.js
@@ -1,62 +1,30 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  block.classList.add('cards', 'long', 'block');
-
-  // Fetch the number of cards to display from the 'count' field (Universal Editor)
-  let numCards = 0;
-  const countField = block.querySelector('[name="count"]');
-  if (countField && countField.value) {
-    numCards = parseInt(countField.value, 10) || 0;
-  }
-  // If numCards is set and there are fewer cards than needed, add empty cards
-  if (numCards > 0) {
-    let ulEl = block.querySelector('ul');
-    if (!ulEl) {
-      ulEl = document.createElement('ul');
-      block.appendChild(ulEl);
-    }
-    const currentCards = ulEl.querySelectorAll('li').length;
-    for (let i = currentCards; i < numCards; i += 1) {
-      const li = document.createElement('li');
-      li.className = 'card';
-      li.innerHTML = `
-        <div class="cards-card-image"><img src="" alt="" /></div>
-        <div class="cards-card-body">
-          <h5>Title</h5>
-          <p>Description</p>
-        </div>
-      `;
-      ulEl.appendChild(li);
-    }
-  }
-
   const ul = document.createElement('ul');
-
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    li.classList.add('masonry-card');
+    moveInstrumentation(row, li);
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+      } else {
+        div.className = 'cards-card-body';
+      }
+    });
+    ul.append(li);
+  });
 
-    const imageCell = row.querySelector('picture');
-    const title = row.querySelector('h1, h2, h3, h4, h5, h6');
-    const description = row.querySelector('p');
-
-    if (imageCell) {
-      const imageDiv = document.createElement('div');
-      imageDiv.className = 'cards-card-image';
-      imageDiv.appendChild(imageCell);
-      li.appendChild(imageDiv);
-    }
-
-    const bodyDiv = document.createElement('div');
-    bodyDiv.className = 'cards-card-body';
-    if (title) bodyDiv.appendChild(title);
-    if (description) bodyDiv.appendChild(description);
-
-    li.appendChild(bodyDiv);
-    ul.appendChild(li);
+  ul.querySelectorAll('picture > img').forEach((img) => {
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPic);
   });
 
   block.textContent = '';
   block.append(ul);
+  // Add masonry classes for styling
+  block.classList.add('long', 'block', 'pinterest-masonry');
 }
